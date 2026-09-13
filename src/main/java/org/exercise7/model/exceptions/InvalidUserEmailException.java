cd24a37608d2f44fa358ee39e563804fb589bcf4
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-13 15:27:47 -0500

    feat/ add messages for exceptions

--- src/main/java/org/exercise7/model/exceptions/InvalidUserEmailException.java
+++ src/main/java/org/exercise7/model/exceptions/InvalidUserEmailException.java
@@ -4,4 +4,9 @@ public class InvalidUserEmailException extends DomainException{
     public InvalidUserEmailException(String message) {
         super(message);
     }
+
+    private static final String MESSAGE_INVALID = "Email is Invalid, it's not a Email";
+    private static final String MESSAGE_INVALID_EMPTY = "Email is Invalid, It's empty.";
+    private static final String MESSAGE_INVALID_NULL = "Email is Invalid, It's empty.";
+    private static final String MESSAGE_EMAIL_EXIST = "Email is Invalid, This email is already registered.";
 }
