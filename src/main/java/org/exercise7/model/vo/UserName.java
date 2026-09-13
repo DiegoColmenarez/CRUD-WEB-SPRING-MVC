acdec45af9a05bbaf74129ebb31fb065885a8056
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-13 15:15:33 -0500

    feat/add pattern validator

--- src/main/java/org/exercise7/model/vo/UserName.java
+++ src/main/java/org/exercise7/model/vo/UserName.java
@@ -1,4 +1,9 @@
 package org.exercise7.model.vo;
 
+import java.util.regex.Pattern;
+
 public record UserName(String value) {
+
+    private static final Pattern NAME_PATTERN =
+            Pattern.compile("^[\\p{L} \\-']+$");
 }
