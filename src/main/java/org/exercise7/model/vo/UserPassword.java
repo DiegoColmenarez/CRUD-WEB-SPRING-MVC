f3b3de0444fb078b10fc8304f4bd464b4ecb0a71
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-13 16:48:28 -0500

    feat/implementation of validateFormat method

--- src/main/java/org/exercise7/model/vo/UserPassword.java
+++ src/main/java/org/exercise7/model/vo/UserPassword.java
@@ -1,9 +1,17 @@
 package org.exercise7.model.vo;
 
+import org.exercise7.model.exceptions.InvalidUserPasswordException;
+
 import java.util.regex.Pattern;
 
 public record UserPassword(String value) {
 
     private static final Pattern PASSWORD_PATTERN =
             Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$");
+
+    private static void validateFormat(String value){
+        if (!PASSWORD_PATTERN.matcher(value).matches()){
+            throw InvalidUserPasswordException.becauseFormatIsInvalid();
+        }
+    }
 }
