163d13c0171ab3ff9cdedb8ce394d6c5c59a3962
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-13 16:39:34 -0500

    feat/ add messages

--- src/main/java/org/exercise7/model/exceptions/InvalidUserPasswordException.java
+++ src/main/java/org/exercise7/model/exceptions/InvalidUserPasswordException.java
@@ -5,4 +5,12 @@ public class InvalidUserPasswordException extends DomainException{
         super(message);
     }
 
+
+    private static final String MESSAGE_INVALID = "The password is invalid, it's empty";
+    private static final String MESSAGE_INVALID_FORMAT = "The password is invalid," +
+            " The password must contain at least: a special character, a number, a lowercase letter, " +
+            "and an uppercase letter. It cannot contain spaces.";
+    private static final String MESSAGE_INVALID_PASSWORD = "Incorrect password";
+
+
 }
