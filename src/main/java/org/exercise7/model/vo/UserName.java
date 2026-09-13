423986220eaf56fdd877bb1509e45c6a9e71b448
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-13 15:17:27 -0500

    feat/implementation of validateNameFormat method

--- src/main/java/org/exercise7/model/vo/UserName.java
+++ src/main/java/org/exercise7/model/vo/UserName.java
@@ -1,9 +1,18 @@
 package org.exercise7.model.vo;
 
+import org.exercise7.model.exceptions.InvalidUserNameException;
+
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public record UserName(String value) {
 
     private static final Pattern NAME_PATTERN =
             Pattern.compile("^[\\p{L} \\-']+$");
+
+    private static void validateNameFormat(String value) {
+        if (!NAME_PATTERN.matcher(value).matches()) {
+            throw InvalidUserNameException.becauseContainsInvalidCharacters();
+        }
+    }
 }
