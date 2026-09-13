80c9ed3acd2aa2bb81a1f5e9356aef787d269eba
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-13 16:25:46 -0500

    implementation of becauseIsNull and becauseValueIsInvalid method

--- src/main/java/org/exercise7/model/exceptions/InvalidUserTypeException.java
+++ src/main/java/org/exercise7/model/exceptions/InvalidUserTypeException.java
@@ -4,4 +4,16 @@ public class InvalidUserTypeException extends DomainException{
     public InvalidUserTypeException(String message) {
         super(message);
     }
+
+    private static final String MESSAGE_INVALID = "User type is invalid";
+    private static final String MESSAGE_INVALID_NULL = "User type is invalid, it's NULL";
+
+    public static InvalidUserTypeException becauseValueIsInvalid(){
+        return new InvalidUserTypeException(MESSAGE_INVALID);
+    }
+
+    public static InvalidUserTypeException becauseIsNull(){
+        return new InvalidUserTypeException(MESSAGE_INVALID_NULL);
+    }
+
 }
