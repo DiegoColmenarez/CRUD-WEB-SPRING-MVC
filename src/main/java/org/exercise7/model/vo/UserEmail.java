9f43b315236b12029bbd97b5ccca1e4df5e20bbe
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-13 15:45:17 -0500

    feat/implementation of validateFormatEmail method

--- src/main/java/org/exercise7/model/vo/UserEmail.java
+++ src/main/java/org/exercise7/model/vo/UserEmail.java
@@ -1,5 +1,7 @@
 package org.exercise7.model.vo;
 
+import org.exercise7.model.exceptions.InvalidUserEmailException;
+
 import java.util.regex.Pattern;
 
 public record UserEmail(String value) {
@@ -8,6 +10,11 @@ public record UserEmail(String value) {
             Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
                     "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$");
 
+    private static void validateFormatEmail(String value){
+        if (!PATTERN_EMAIL.matcher(value).matches()){
+            throw InvalidUserEmailException.becauseFormatIsInvalid();
+        }
+    }
 
 
 }
