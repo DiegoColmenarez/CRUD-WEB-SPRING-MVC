96c1ba9f8db1765b83b437d62980c21122ae2003
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-13 15:29:02 -0500

    feat/implementation of becauseFormatIsInvalid method

--- src/main/java/org/exercise7/model/exceptions/InvalidUserEmailException.java
+++ src/main/java/org/exercise7/model/exceptions/InvalidUserEmailException.java
@@ -7,6 +7,10 @@ public class InvalidUserEmailException extends DomainException{
 
     private static final String MESSAGE_INVALID = "Email is Invalid, it's not a Email";
     private static final String MESSAGE_INVALID_EMPTY = "Email is Invalid, It's empty.";
-    private static final String MESSAGE_INVALID_NULL = "Email is Invalid, It's empty.";
+    private static final String MESSAGE_INVALID_NULL = "Email is Invalid, It's NULL.";
     private static final String MESSAGE_EMAIL_EXIST = "Email is Invalid, This email is already registered.";
+
+    public static InvalidUserEmailException becauseFormatIsInvalid(){
+        return new InvalidUserEmailException(MESSAGE_INVALID);
+    }
 }
