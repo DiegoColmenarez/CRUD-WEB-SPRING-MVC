58642180858b4b8ec79cf198e0fe4723032fb5db
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-14 18:59:06 -0500

    chore/ add messages

--- src/main/java/org/exercise7/model/exceptions/UserNotFoundException.java
+++ src/main/java/org/exercise7/model/exceptions/UserNotFoundException.java
@@ -4,4 +4,7 @@ public class UserNotFoundException extends DomainException{
     public UserNotFoundException(String message) {
         super(message);
     }
+
+    private static final String MESSAGE_USER_NOT_EXIST = "The user id: '%d' don't exist";
+    private static final String MESSAGE_USER_EMAIL_NOT_EXIST = "The user with email: '%s' don't exist";
 }
