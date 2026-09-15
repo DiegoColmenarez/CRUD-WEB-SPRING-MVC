b313fe3f74a561d85f7c07ad5a0373834227ad0a
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-14 22:46:20 -0500

    feat/implementation of becauseEmailAlredyExist method

--- src/main/java/org/exercise7/model/exceptions/EmailAlreadyExistsException.java
+++ src/main/java/org/exercise7/model/exceptions/EmailAlreadyExistsException.java
@@ -4,4 +4,11 @@ public class EmailAlreadyExistsException extends DomainException{
     public EmailAlreadyExistsException(String message) {
         super(message);
     }
+
+    private static final String MESSAGE_EMAIL = "The email address you are trying to enter" +
+            " already exists in the database.";
+
+    public static EmailAlreadyExistsException becauseEmailAlredyExist(){
+        return new EmailAlreadyExistsException(MESSAGE_EMAIL);
+    }
 }
