34dd733e7b33d715c2e67f61e72b397f2fdfac77
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-15 00:51:06 -0500

    feat/add constructor in UserController class

--- src/main/java/org/exercise7/controller/UserController.java
+++ src/main/java/org/exercise7/controller/UserController.java
@@ -1,9 +1,15 @@
 package org.exercise7.controller;
 
+import org.exercise7.model.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
