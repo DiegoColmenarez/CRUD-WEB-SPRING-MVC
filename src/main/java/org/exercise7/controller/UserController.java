8153b734938294b16380cd200e0840da9ebdcdf2
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-15 00:55:47 -0500

    feat/implementation of processRegistration method

--- src/main/java/org/exercise7/controller/UserController.java
+++ src/main/java/org/exercise7/controller/UserController.java
@@ -1,10 +1,14 @@
 package org.exercise7.controller;
 
+import jakarta.validation.Valid;
 import org.exercise7.model.entity.User;
 import org.exercise7.model.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +24,17 @@ public class UserController {
     public String showRegistrationForm(Model model) {
         model.addAttribute("user", new User());
         return "users/formulario";
+    }
+
+    @PostMapping("/guardar")
+    public String processRegistration(
+            @Valid @ModelAttribute("user") User user,
+            BindingResult result,
+            Model model) {
+        if (result.hasErrors()) {
+            return "users/formulario";
+        }
+        userService.registerUser(user);
+        return "redirect:/usuarios/lista";
     }
 }
