4e867476faca31bf8e66dc7d11e739b9612e3ed0
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-14 20:43:45 -0500

    feat/add UserRepository interface

---
+++ src/main/java/org/exercise7/model/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package org.exercise7.model.repository;
+
+import org.exercise7.model.entity.User;
+import org.exercise7.model.enums.TypeUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByName(String name);
+
+    Optional<User> findByLastName(String lastName);
+
+    Optional<User> findByType(TypeUser type);
+
+    @Modifying
+    @Query("UPDATE User u SET u.password = :password WHERE u.id = :id")
+    void updatePassword(@Param("id") Long id, @Param("password") String password);
+}
