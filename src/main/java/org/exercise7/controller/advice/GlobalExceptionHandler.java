5b42a361e92d30e42f15e26881c71e651d8bc81f
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-15 00:24:39 -0500

    feat/implementation of handleUserNotFoundException

--- src/main/java/org/exercise7/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/org/exercise7/controller/advice/GlobalExceptionHandler.java
@@ -1,14 +1,22 @@
 package org.exercise7.controller.advice;
 
+import org.exercise7.model.exceptions.UserNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-
+    @ExceptionHandler(UserNotFoundException.class)
+    public String handleUserNotFoundException(UserNotFoundException ex, Model model) {
+        logger.warn("Recurso no encontrado: {}", ex.getMessage());
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/404";
+    }
 
 }
