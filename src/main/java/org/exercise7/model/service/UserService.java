df61d4f7b590b728f46aba2fb59b93f4d32b5a6c
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-14 22:43:04 -0500

    feat/implementation of registerUser method

--- src/main/java/org/exercise7/model/service/UserService.java
+++ src/main/java/org/exercise7/model/service/UserService.java
@@ -1,8 +1,10 @@
 package org.exercise7.model.service;
 
+import org.exercise7.model.entity.User;
 import org.exercise7.model.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -14,5 +16,14 @@ public class UserService {
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("The email is already registered");
+        }
+         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 }
