0fa594c42499866fdb07c59926bad3535d10858a
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-16 16:43:53 -0500

    feat/ implementation of deleteComputer method

--- src/main/java/org/exercise7/controller/ComputerController.java
+++ src/main/java/org/exercise7/controller/ComputerController.java
@@ -10,10 +10,7 @@ import org.exercise7.model.service.ComputerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/computers")
@@ -60,4 +57,10 @@ public class ComputerController {
         model.addAttribute("computers", computerService.findAllComputers());
         return "computers/list";
     }
+    @PostMapping("/delete/{id}")
+    public String deleteComputer(@PathVariable("id") Long id) {
+        computerService.deleteComputer(id);
+        return "redirect:/computers/list";
+    }
+
 }
