1dad90072a684a5e06821903512145a1f6370f61
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-15 20:37:14 -0500

    refactor/new implementation of updateUser method

--- src/main/java/org/exercise7/controller/UserController.java
+++ src/main/java/org/exercise7/controller/UserController.java
@@ -3,6 +3,7 @@ package org.exercise7.controller;
 import jakarta.validation.Valid;
 import org.exercise7.model.entity.User;
 import org.exercise7.model.enums.TypeUser;
+import org.exercise7.model.exceptions.EmailAlreadyExistsException;
 import org.exercise7.model.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -68,13 +69,14 @@ public class UserController {
     @PostMapping("/actualizar/{id}")
     public String updateUser(@PathVariable Long id,
                              @ModelAttribute("user") User user,
-                             BindingResult result,
                              Model model) {
-        if (result.hasErrors()) {
+        try {
+            userService.updateUserBasicInfo(id, user.getName(), user.getLastName(), user.getEmail());
+            return "redirect:/usuarios/lista";
+        } catch (EmailAlreadyExistsException ex) {
+            model.addAttribute("error", "El correo ingresado ya pertenece a otro usuario.");
             user.setId(id);
             return "users/formulario";
         }
-        userService.updateUserBasicInfo(id, user.getName(), user.getLastName(), user.getEmail());
-        return "redirect:/usuarios/lista";
     }
 }
