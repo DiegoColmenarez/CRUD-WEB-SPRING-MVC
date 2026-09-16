58bed71b6a72dd36d9f46b0a09f708571575658b
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-16 16:33:56 -0500

    feat/implementation of processRegistration method

--- src/main/java/org/exercise7/controller/ComputerController.java
+++ src/main/java/org/exercise7/controller/ComputerController.java
@@ -1,14 +1,18 @@
 package org.exercise7.controller;
 
+import jakarta.validation.Valid;
 import org.exercise7.model.entity.Computer;
 import org.exercise7.model.enums.Category;
 import org.exercise7.model.enums.DiskTechnology;
 import org.exercise7.model.enums.RamTechnology;
+import org.exercise7.model.exceptions.DomainException;
 import org.exercise7.model.service.ComputerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,5 +36,22 @@ public class ComputerController {
     public String showRegistrationForm(Model model) {
         model.addAttribute("computer", new Computer());
         return "computers/form";
+    }
+
+    @PostMapping("/save")
+    public String processRegistration(
+            @Valid @ModelAttribute("computer") Computer computer,
+            BindingResult result,
+            Model model) {
+        if (result.hasErrors()) {
+            return "computers/form";
+        }
+        try {
+            computerService.saveComputer(computer);
+        } catch (DomainException e) {
+            model.addAttribute("businessError", e.getMessage());
+            return "computers/form";
+        }
+        return "redirect:/computers/list";
     }
 }
