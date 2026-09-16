0b0aaeb2c71db5dc5cd5f5435554b7be34b95af7
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-16 06:46:59 -0500

    refactor/ now UserNotFoundException extends ResourceNotFoundException

--- src/main/java/org/exercise7/model/exceptions/UserNotFoundException.java
+++ src/main/java/org/exercise7/model/exceptions/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package org.exercise7.model.exceptions;
 
-public class UserNotFoundException extends DomainException{
+public class UserNotFoundException extends ResourceNotFoundException {
     public UserNotFoundException(String message) {
         super(message);
     }
