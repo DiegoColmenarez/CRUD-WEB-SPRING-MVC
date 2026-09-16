0d0e96de7f41fa2da9539288edc4155f719c8a8e
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-16 16:09:16 -0500

    feat/add constructor ComputerController

--- src/main/java/org/exercise7/controller/ComputerController.java
+++ src/main/java/org/exercise7/controller/ComputerController.java
@@ -1,9 +1,18 @@
 package org.exercise7.controller;
 
+import org.exercise7.model.service.ComputerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/computers")
 public class ComputerController {
+
+    private final ComputerService computerService;
+
+    public ComputerController(ComputerService computerService) {
+        this.computerService = computerService;
+    }
+
+
 }
