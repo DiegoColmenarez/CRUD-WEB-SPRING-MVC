f2069ab6b6a355134041ad7c87dfb721d231ac01
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-15 23:48:56 -0500

    feat/ add findValidByCode and query

---
+++ src/main/java/org/exercise7/model/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,17 @@
+package org.exercise7.model.repository;
+
+import org.exercise7.model.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    @Query("SELECT t FROM PasswordResetToken t " +
+            "WHERE t.code = :code AND t.used = false AND t.expiresAt > CURRENT_TIMESTAMP " +
+            "ORDER BY t.createdAt DESC")
+    List<PasswordResetToken> findValidByCode(@Param("code") String code);
+
+}
